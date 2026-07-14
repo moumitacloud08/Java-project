6b42261986360da0c655156bb1d4246ae7682a2a
Moumita Ray <moumitacloud08@gmail.com>
2026-07-14 20:59:39 +0530

    BFS

---
+++ ProgramPractise_01/src/main/java/org/leetcode/tree/traversal/BFS/Main.java
@@ -0,0 +1,20 @@
+package org.leetcode.tree.traversal.BFS;
+
+public class Main {
+
+    public static void main(String[] args){
+        BinarySearchTree myBST = new BinarySearchTree();
+        myBST.insert(47);
+        myBST.insert(21);
+        myBST.insert(76);
+        myBST.insert(18);
+        myBST.insert(27);
+        myBST.insert(52);
+        myBST.insert(82);
+
+        System.out.println(myBST.traversal());
+
+    }
+
+
+}
