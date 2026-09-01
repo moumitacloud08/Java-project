f65b5003c8e9f726ab0c024423c9bb9749c04ada
Moumita Ray <moumitacloud08@gmail.com>
2026-09-01 17:14:14 +0530

    Find middle

---
+++ ProgramPractise_01/src/main/java/org/leetcode/LinkedList/excercise1/Main.java
@@ -0,0 +1,25 @@
+package org.leetcode.LinkedList.excercise1;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        LinkedList myLinkedList = new LinkedList(1);
+        myLinkedList.append(2);
+        myLinkedList.append(3);
+        myLinkedList.append(4);
+        myLinkedList.append(5);
+        
+        myLinkedList.printList();
+        System.out.println();
+        
+        
+
+
+        System.out.println("Middle Value :: "+myLinkedList.findMiddle().value);
+
+
+       
+
+    }}
