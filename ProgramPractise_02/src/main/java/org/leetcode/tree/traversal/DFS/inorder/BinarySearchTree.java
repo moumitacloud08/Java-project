31f5637e845d6a15f294e534760791b551f35329
Moumita Ray <moumitacloud08@gmail.com>
2026-09-17 14:37:04 +0530

    new prog added

---
+++ ProgramPractise_02/src/main/java/org/leetcode/tree/traversal/DFS/inorder/BinarySearchTree.java
@@ -0,0 +1,93 @@
+package org.leetcode.tree.traversal.DFS.inorder;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+
+public class BinarySearchTree {
+
+    public Node root;
+
+    public static class Node {
+        public int value;
+        public Node left;
+        public Node right;
+
+        private Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public Node getRoot() {
+        return root;
+    }
+
+    public boolean insert(int value) {
+        Node newNode = new Node(value);
+        if (root == null) {
+            root = newNode;
+            return true;
+        }
+        Node temp = root;
+        while (true) {
+            if (newNode.value == temp.value) return false;
+            if (newNode.value < temp.value) {
+                if (temp.left == null) {
+                    temp.left = newNode;
+                    return true;
+                }
+                temp = temp.left;
+            } else {
+                if (temp.right == null) {
+                    temp.right = newNode;
+                    return true;
+                }
+                temp = temp.right;
+            }
+        }
+    }
+
+    public boolean contains(int value) {
+        if (root == null) return false;
+        Node temp = root;
+        while (temp != null) {
+            if (value < temp.value) {
+                temp = temp.left;
+            } else if (value > temp.value) {
+                temp = temp.right;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public List<Integer> DFSInOrder(){
+    	ArrayList<Integer> result = new ArrayList<Integer>();
+    	
+        class Traverse{
+        	
+        	Traverse(Node currentNode){
+        		
+        		
+        		if(currentNode.left!= null) {
+        			new Traverse(currentNode.left);
+        		}
+        		result.add(currentNode.value);
+        		
+        		if(currentNode.right!= null) {
+        			new Traverse(currentNode.right);
+        		}
+        		
+        		
+        	}
+        }
+        new Traverse(root);
+		return result;
+    	
+    }
+    
+
+}
---
+++ ProgramPractise_02/src/main/java/org/leetcode/tree/traversal/DFS/inorder/Main.java
@@ -0,0 +1,21 @@
+package org.leetcode.tree.traversal.DFS.inorder;
+
+public class Main {
+
+    public static void main(String[] args){
+        BinarySearchTree myBST = new BinarySearchTree();
+        myBST.insert(47);
+        myBST.insert(21);
+        myBST.insert(76);
+        myBST.insert(18);
+        myBST.insert(27);
+        myBST.insert(52);
+        myBST.insert(82);
+
+        System.out.println(myBST.DFSInOrder());
+        //[18, 21, 27, 47, 52, 76, 82]
+
+    }
+
+
+}
